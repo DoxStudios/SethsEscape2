c1c1c437a3c98b6fd10e4e7299794c7f87254390
FruitJuic3 <connor.townsend312@gmail.com>
2023-02-25 14:48:34 -0800

    Ported to unity

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 45f;
+    public float jumpForce = 60f;
+    public float doubleJumpForce = 45f;
+    public bool doubleJump = true;
+    public LayerMask ground;
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    public GameObject groundCheck;
+
+    float groundedRadius = 0.3f;
+    bool isGrounded = true;
+    bool doubleJumpRemaining = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+       isGrounded = checkGrounded();
+       if(isGrounded)
+       {
+            doubleJumpRemaining = true;
+       }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector3 move = new Vector3(horizontal, 0, 0);
+        if(horizontal < 0)
+        {
+            sr.flipX = true;
+        }
+        else if(horizontal > 0)
+        {
+            sr.flipX = false;
+        }
+        transform.position += move * speed * Time.deltaTime;
+
+        if((isGrounded || doubleJumpRemaining) && Input.GetButtonDown("Jump"))
+        {
+            if(!isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+                doubleJumpRemaining = false;
+                rb.AddForce(transform.up * doubleJumpForce, ForceMode2D.Impulse);
+            }
+            else
+            {
+                isGrounded = false;
+                rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            }
+            
+        }
+    }
+
+    bool checkGrounded()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.transform.position, groundedRadius, ground);
+        for (int i = 0; i < colliders.Length; i++) {
+            if (colliders[i].gameObject != gameObject) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
