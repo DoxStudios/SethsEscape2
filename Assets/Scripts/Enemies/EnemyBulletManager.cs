7e8a551d93476f506d86cec90930684d3b95b7c1
Definitely the real Obama <theunfunnyone@doxstudios.net>
2023-07-24 17:36:39 -0700

    Created enemy bullet

---
+++ Assets/Scripts/Enemies/EnemyBulletManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletManager : MonoBehaviour
+{
+    public float damage;
+    public float knockbackMultiplier;
+    public float knockbackTimeMultiplier;
+    public float stunTimeMultiplier;
+    public EnemyStatsManager esm;
+    public int pierceLevel;
+    public float survivalTime;
+
+    void Update()
+    {
+        survivalTime -= Time.deltaTime;
+
+        if(survivalTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            PlayerStatsManager psm = col.gameObject.GetComponent<PlayerStatsManager>();
+            psm.Damage(damage, transform, esm.outgoingKnockbackAmount * knockbackMultiplier, esm.outgoingKnockbackTime * knockbackTimeMultiplier, esm.outgoingStunTime * stunTimeMultiplier);
+        }
+        else if(col.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+
+        pierceLevel -= 1;
+        if(pierceLevel == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
